d4a3f8dafe82d32931594df9020d087efe73ed50
Akkun <akkunlab@gmail.com>
2022-08-22 10:49:36 +0900

    Add OSC to openFrameworks

--- openFrameworks/src/ofApp.cpp
+++ openFrameworks/src/ofApp.cpp
@@ -1,13 +1,49 @@
+﻿#include <iostream>
+#include <Windows.h>
 #include "ofApp.h"
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    ofBackground(64); // 背景色を設定
+    ofSetFrameRate(60); // フレームレートを設定
+    ofSetWindowTitle("Art_Project"); // ウィンドウタイトルを設定
 
+    /* ==============================
+      OSC
+    ============================== */
+    receiver.setup(PORT);  // 受信機を設定
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
 
+    /* ==============================
+       OSC
+    ============================== */
+
+    // メッセージが送られてくるまで待機
+    while (receiver.hasWaitingMessages()) {
+
+        // 次のメッセージを取得
+        ofxOscMessage m;
+        receiver.getNextMessage(m);
+
+        // ログ出力
+        // std::cout << m << endl;
+
+        // アドレス分岐処理
+        if (m.getAddress() == "/mouse/button") {
+
+            if (m.getArgAsString(0) == "up") {
+
+                // ログ出力
+                std::cout << m.getArgAsString(0) << endl;
+
+                // ソフトを起動し、音源再生
+                ShellExecute(NULL, "open", "filePath", NULL, NULL, SW_SHOWNORMAL);
+            }
+        }
+    }
 }
 
 //--------------------------------------------------------------
@@ -26,7 +62,7 @@ void ofApp::keyReleased(int key){
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y){
 
 }
 
@@ -66,6 +102,6 @@ void ofApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){
 
 }
